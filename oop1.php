6a7cabebd8bd21734bf187183084d31051977825
sazam <datacanvassbd@gmail.com>
2022-08-31 20:29:11 +0600

    Added gitignore

---
+++ oop1.php
@@ -0,0 +1,30 @@
+<?php 
+class Human{
+    function sayHi(){
+        echo "Salam <br>";
+    }
+}
+
+class Cat {
+    function sayHi(){
+        echo "Meow <br>";
+    }
+}
+
+
+class Dog {              // Class
+    function sayHi(){
+        echo "Woof <br>";
+    }
+}
+
+$h1 = new Human();  // Object
+
+$c1 = new Cat();
+
+$d1 = new Dog();
+
+
+$h1 -> sayHi();  // Method Property
+$c1-> sayHi();
+$d1-> sayHi();
