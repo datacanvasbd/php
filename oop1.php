9fa489656363296b8fe2e2444a1d11e196eac1eb
sazam <datacanvassbd@gmail.com>
2022-09-01 05:53:00 +0600

    Updated

--- oop1.php
+++ oop1.php
@@ -1,8 +1,14 @@
 <?php 
 class Human{
+    public $name; // Property of Human class
     function sayHi(){
-        echo "Salam <br>";
+        echo " Salam <br>";
+        $this->sayName();
     }
+    function sayName(){
+        echo "My Name is {$this->name} <br>";
+    }
+
 }
 
 class Cat {
@@ -19,12 +25,21 @@ class Dog {              // Class
 }
 
 $h1 = new Human();  // Object
-
+$h1->name = " Azam "; // Set
+$h2 = new Human();
+$h2->name = "Rafi";
 $c1 = new Cat();
 
 $d1 = new Dog();
 
-
+# echo $h1->name;  // Get
 $h1 -> sayHi();  // Method Property
-$c1-> sayHi(); // Method Property
+# $h1->sayName();
+
+# echo $h2->name;  // Get
+$h2 -> sayHi();  // Method Property
+# $h2->sayName();
+
+
+$c1-> sayHi();
 $d1-> sayHi();
